f0305e9cf277a655e122d0b58c3105bcf15eee2e
YuliaSukach <35064670+YuliaSukach@users.noreply.github.com>
2022-10-15 00:14:19 +0200

    fixed hw_05_04 task

--- less_05/hw_05_04.py
+++ less_05/hw_05_04.py
@@ -5,30 +5,43 @@
 
 from datetime import datetime
 
+factorial_num = 20
 
-def calc_time(func):
-    def inner():
-        start_time = datetime.now()
-        result = func
-        print((datetime.now() - start_time) * 1000000.0)
+
+def calc_fibonacci_num(func):
+    start_time = datetime.now()
+
+    def inner(n):
+        result = func(n)
+        if factorial_num == n:
+            print((datetime.now() - start_time) * 1000000.0)
         return result
 
     return inner
 
 
+@calc_fibonacci_num
 def fibonacci_num(n):
     if n in (1, 2):
         return 1
     return fibonacci_num(n - 1) + fibonacci_num(n - 2)
 
 
+def calc_func_binet(func):
+    def inner(num):
+        start_time = datetime.now()
+        result = func(num)
+        print((datetime.now() - start_time) * 1000000.0)
+        return result
+
+    return inner
+
+
+@calc_func_binet
 def func_binet(n):
     const = (1 + 5 ** 0.5) / 2
-    return int((const ** n - (-const) ** (-n)) / (2 * const - 1))
+    return round((const ** n - (-const) ** (-n)) / (2 * const - 1))
 
 
-func_binet_time = calc_time(func_binet(40))
-func_binet_time()
-
-fibonacci_time = calc_time(fibonacci_num(40))
-fibonacci_time()
+fibonacci_num(factorial_num)
+func_binet(factorial_num)
